21fb519f701e1bf7f606fcce1abc8d9b5b2a84ce
David Spector <davidspector9@gmail.com>
2024-02-06 22:09:22 -0800

    2d CEUS works for const ROIs

--- paramap.py
+++ paramap.py
@@ -93,6 +93,86 @@ def generate_TIC(window, mask, times, compression, voxelscale):
         TICz[:,1]=TICz[:,1]-np.min(TICz[:,1]);
     return TICz;
 
+def generate_TIC_2d(window, mask, times, compression, voxelscale):
+    TICtime=times;TIC=[]; 
+    bool_mask = np.array(mask, dtype=bool)
+    for t in range(0,window.shape[2]):
+        tmpwin = window[:,:,t];      
+        TIC.append(np.around(np.exp(tmpwin[bool_mask]/compression).mean()/voxelscale, decimals=1));
+        # TIC.append(np.exp(tmpwin[bool_mask]/compression).mean()*voxelscale);
+        # TIC.append(np.around((tmpwin[bool_mask]/compression).mean()*voxelscale, decimals=1)); 
+    TICz = np.array([TICtime,TIC]).astype('float64'); TICz = TICz.transpose();
+    TICz[:,1]=TICz[:,1]-np.mean(TICz[0:2,1]);#Substract noise in TIC before contrast.
+    if TICz[np.nan_to_num(TICz)<0].any():#make the smallest number in the TIC 0.
+        TICz[:,1]=TICz[:,1]+np.abs(np.min(TICz[:,1]));
+    else:
+        TICz[:,1]=TICz[:,1]-np.min(TICz[:,1]);
+    return TICz;
+
+# def generate_TIC_mc(window, bboxes, times, compression):
+#     TICtime = []
+#     TIC = []
+#     areas = []
+#     for t in range(0, window.shape[0]):
+#         if bboxes[t] is not None:
+#             tmpwin = window[t]
+#             bool_mask = np.zeros(tmpwin.shape, dtype=bool)
+#             x0, y0, x_len, y_len = bboxes[t]
+#             if y0 + y_len >= bool_mask.shape[0]:
+#                 y_len = bool_mask.shape[0] - y0 - 1
+#             if x0 + x_len >= bool_mask.shape[1]:
+#                 x_len = bool_mask.shape[0] - x0 - 1
+#             bool_mask[y0 : y0 + y_len, x0 : x0 + x_len] = True
+#             # for x in range(x_len):
+#             #     bool_mask[y0,x] = True
+#             #     bool_mask[y0+y_len, x] = True
+#             # for y in range(y_len):
+#             #     bool_mask[y, x0] = True
+#             #     bool_mask[y, x0+x_len] = True
+#             # bool_mask = binary_fill_holes(bool_mask)
+#             numPoints = len(np.where(bool_mask > 0)[0])
+#             TIC.append(np.exp(tmpwin[bool_mask] / compression).mean())
+#             TICtime.append(times[t])
+#             areas.append(numPoints)
+
+#     TICz = np.array([TICtime, TIC]).astype("float64")
+#     TICz = TICz.transpose()
+#     TICz[:, 1] = TICz[:, 1] - np.mean(
+#         TICz[0:2, 1]
+#     )  # Subtract noise in TIC before contrast
+#     if TICz[np.nan_to_num(TICz) < 0].any():  # make the smallest number in TIC 0
+#         TICz[:, 1] = TICz[:, 1] + np.abs(np.min(TICz[:, 1]))
+#     else:
+#         TICz[:, 1] = TICz[:, 1] - np.min(TICz[:, 1])
+#     return TICz, np.round(np.mean(areas), decimals=2)
+
+def generate_TIC_2d_MC(window, mask, times, compression):
+    TICtime = []
+    TIC = []
+    areas = []
+    summed_window = np.transpose(np.sum(np.squeeze(window), axis=3))
+    for t in range(0, mask.shape[2]):
+        tmpwin = summed_window[t]
+        bool_mask = np.array(mask[t]).astype(bool)
+        numPoints = len(np.where(bool_mask > 0)[0])
+        if numPoints == 0:
+            continue
+        TIC.append(np.exp(tmpwin[bool_mask] / compression).mean())
+        TICtime.append(times[t])
+        areas.append(numPoints)
+
+    TICz = np.array([TICtime, TIC]).astype("float64")
+    TICz = TICz.transpose()
+    TICz[:, 1] = TICz[:, 1] - np.mean(
+        TICz[0:2, 1]
+    )  # Subtract noise in TIC before contrast
+    if TICz[np.nan_to_num(TICz) < 0].any():  # make the smallest number in TIC 0
+        TICz[:, 1] = TICz[:, 1] + np.abs(np.min(TICz[:, 1]))
+    else:
+        TICz[:, 1] = TICz[:, 1] - np.min(TICz[:, 1])
+    return TICz
+
+
 # def generate_TIC(window, times, compression,voxelscale):
 #     TICtime=[];TIC=[];
 #     for t in range(0,times.shape[0]):
@@ -106,6 +186,167 @@ def generate_TIC(window, mask, times, compression, voxelscale):
 #     else:
 #         TICz[:,1]=TICz[:,1]-np.min(TICz[:,1]);
 #     return TICz;
+
+# def organize_points_by_time(pointsPlotted: np.array) -> list:
+#     cur_time = pointsPlotted[0,2]
+#     organized_points = []
+#     cur_time_points = []
+#     for point in pointsPlotted:
+#         if point[2] == cur_time:
+#             cur_time_points.append(point)
+#         else:
+#             organized_points.append(cur_time_points)
+#             cur_time = point[2]
+#             cur_time_points = [point]
+#     organized_points.append(cur_time_points)
+#     return organized_points
+
+# def get_bboxes(pointsPlotted: np.array) -> list:
+#     organized_points = organize_points_by_time(pointsPlotted)
+
+#     bboxes = []
+#     for time_sector in organized_points:
+#         x0 = np.min(time_sector[:,0])
+#         w = np.max(time_sector[:,0]) - x0
+#         y0 = np.min(time_sector[:,1])
+#         h = np.max(time_sector[:,1]) - y0
+#         bboxes.append((x0, y0, w, h))
+
+#     return bboxes
+
+def get_bbox(x_coords: np.array, y_coords: np.array, windSize_x: int, windSize_y: int) -> np.array:
+    x0 = np.min(x_coords)
+    y0 = np.min(y_coords)
+    w = np.max(x_coords)-x0
+    h = np.max(y_coords)-y0
+
+    pix_x0s = np.arange(x0, x0+w, windSize_x)[:-1]
+    pix_y0s = np.arange(y0, y0+h, windSize_y)[:-1]
+    pix_bboxes = np.transpose(np.meshgrid(pix_x0s, pix_y0s))
+    pix_bboxes = np.pad(pix_bboxes, [(0,0), (0,0), (0,2)], mode='constant', constant_values=0)
+    pix_bboxes[:,:,2] = windSize_x
+    pix_bboxes[:,:,3] = windSize_y
+    return pix_bboxes
+
+def paramap2d(img, mask, res, time, tf, compressfactor, windSize_x, windSize_y, mc):
+    # windSize_x = 1; windSize_y = 1; windSize_z = 1
+    print('*************************** Starting Parameteric Map *****************************')
+    # print('Prep For Loop:');print(str(datetime.now()));
+    # start_time = datetime.now()
+    #1a. Windowing and image info
+    global windSize, voxelscale, compression, imgshape, timeconst, times, xlist, ylist, zlist, typefit;
+    windSize = (windSize_x, windSize_y);
+    voxelscale = res[0]*res[1]*res[2];
+    compression = compressfactor; 
+    imgshape = img.shape;
+    typefit = tf;
+    #img = img - np.mean(img[:,0:4,:,:,:,:],axis=1);img[img < 1]=0;
+
+    # Make expected calculation time
+
+    #1b. Creat time point and position lists
+    timeconst = time;#time/(img.shape[1]+1);
+    times = [i*time for i in range(1, img.shape[2]+1)];
+
+    if mc:
+        bbox_shape_x = 0
+        bbox_shape_y = 0
+        pixel_bboxes = []
+        for t in range(mask.shape[2]):
+            xmask, ymask = np.where(mask[:,:,t]>0)
+            if len(xmask):
+                pixel_bboxes.append(get_bbox(xmask, ymask, windSize_x, windSize_y))
+                if not bbox_shape_x:
+                    bbox_shape_x = pixel_bboxes[-1].shape[0]
+                    bbox_shape_y = pixel_bboxes[-1].shape[1]
+            else:
+                pixel_bboxes.append(None)
+
+        final_map = np.zeros((img.shape[0], img.shape[1], img.shape[2], 5))
+        for x in range(bbox_shape_x):
+            for y in range(bbox_shape_y):
+                segMask = np.zeros((img.shape[2], img.shape[1], img.shape[0]))
+                for t, bbox in enumerate(pixel_bboxes):
+                    if bbox is not None:
+                        x0, y0, x_len, y_len = bbox[x,y]
+                        segMask[t, y0 : y0 + y_len, x0 : x0 + x_len] = 1
+
+                cur_TIC = generate_TIC_2d_MC(img, segMask, times, compression)
+                normalizer = np.max(cur_TIC[:,1]);
+                cur_TIC[:,1] = cur_TIC[:,1]/normalizer;
+
+                # Bunch of checks
+                if np.isnan(np.sum(cur_TIC[:,1])):
+                    print('STOPPED:NaNs in the VOI')
+                    return;
+                if np.isinf(np.sum(cur_TIC[:,1])):
+                    print('STOPPED:InFs in the VOI')
+                    return;
+
+                # Do the fitting
+                try:
+                    params, popt, wholecurve = data_fit(cur_TIC,normalizer, timeconst);
+                    index_points = np.transpose(np.where(np.transpose(segMask)>0))
+                    final_map[index_points] = params
+                except RuntimeError:
+                    # params = np.array([-1, np.max(cur_TIC[:,1]), -1, -1])
+                    pass
+                
+        print('Paraloop ended:')
+        return final_map
+    
+    try:
+        xmask, ymask, _ = np.where(mask>0)
+        xlist = np.arange(min(xmask), max(xmask)+windSize_x, windSize_x)
+        ylist = np.arange(min(ymask), max(ymask)+windSize_y, windSize_y)
+    except:
+        print("Voxel dimensions too small! Try larger values")
+        exit(1)
+    final_map = np.zeros([img.shape[0], img.shape[1], 5]).astype(np.double)
+    summed_img = np.sum(np.squeeze(img), axis=3)
+    for x_base in range(len(xlist)):
+        for y_base in range(len(ylist)):
+            cur_mask = np.zeros([img.shape[0], img.shape[1]])
+            indices = []
+            for x in range(windSize[0]):
+                cur_index = []
+                cur_index.append(xlist[x_base]+x)
+                for y in range(windSize[1]):
+                    cur_index.append(ylist[y_base]+y)
+                    indices.append(cur_index.copy())
+                    cur_index.pop()
+                cur_index.pop()
+            sig_indices = False
+            for i in indices:
+                if max(summed_img[i[0],i[1]]) != 0:
+                    cur_mask[i[0],i[1]] = 1
+                    sig_indices = True
+            if not sig_indices:
+                continue
+            cur_TIC = generate_TIC_2d(summed_img, cur_mask, times, 24.9,  voxelscale)
+            normalizer = np.max(cur_TIC[:,1]);
+            cur_TIC[:,1] = cur_TIC[:,1]/normalizer;
+
+            # Bunch of checks
+            if np.isnan(np.sum(cur_TIC[:,1])):
+                print('STOPPED:NaNs in the VOI')
+                return;
+            if np.isinf(np.sum(cur_TIC[:,1])):
+                print('STOPPED:InFs in the VOI')
+                return;
+
+            # Do the fitting
+            try:
+                params, popt, wholecurve = data_fit(cur_TIC,normalizer, timeconst);
+                for i in indices:
+                    final_map[i[0],i[1]] = params
+            except RuntimeError:
+                # params = np.array([-1, np.max(cur_TIC[:,1]), -1, -1])
+                pass
+
+    print('Paraloop ended:')#;print(str(datetime.now()));
+    return final_map;
+
 
 def paramap(img, xmask, ymask, zmask, res, time, tf, compressfactor, windSize_x, windSize_y, windSize_z):
     # windSize_x = 1; windSize_y = 1; windSize_z = 1
@@ -195,6 +436,79 @@ def paramap(img, xmask, ymask, zmask, res, time, tf, compressfactor, windSize_x,
     # windowDepthValue = sys.argv[5] # float
     # destinationPath = Path(sys.argv[6]) # string
 
+def main2d(imPath, maskPath, windowHeightValue, windowWidthValue, destinationPath, bmodeSeparate=0, mc=0, manual_fps=-1):
+    start = time.time()
+    if not os.path.exists(imPath):
+        print("The image file doesn't exist")
+        raise SystemExit(1)
+    if not (imPath.endswith('.nii') or imPath.endswith('.nii.gz')):
+        print("CEUS image must be in NIFTI format")
+        raise SystemExit(1)
+    
+    if not os.path.exists(maskPath):
+        print("The mask file doesn't exist")
+        raise SystemExit(1)
+    if not (maskPath.endswith('.nii') or maskPath.endswith('.nii.gz')):
+        print("CEUS image must be in NIFTI format")
+        raise SystemExit(1)
+    
+    nibImg = nib.load(imPath, mmap=False)
+    header = nibImg.header['pixdim'] # [dims, voxel dims (3 vals), timeconst, 0, 0, 0]
+    dataNibImg = nibImg.get_fdata()
+    image = dataNibImg.astype(np.uint8)
+
+    nibMask = nib.load(maskPath, mmap=False)
+    dataNibMask = nibMask.get_fdata()
+    mask = dataNibMask.astype(np.uint8)
+    if bmodeSeparate:
+        mask = mask[:,:image.shape[1]]
+
+    xlist, ylist, fitted_times = np.where( mask > 0)
+    pointsPlotted = np.transpose([xlist, ylist, fitted_times])
+
+    compressValue = 24.9 # hardcoded for now
+    if manual_fps == -1:
+        fps = header[4]
+    else:
+        fps = (1/manual_fps)
+
+    masterParamap = paramap2d(image, mask, header[1:4], fps, 'BolusLognormal', compressValue, int(windowHeightValue/header[1]), int(windowWidthValue/header[2]), mc)
+    # maxAuc = 0
+    # minAuc = 99999999
+    # maxPe = 0
+    # minPe = 99999999
+    # maxTp = 0
+    # minTp = 99999999
+    # maxMtt = 0
+    # minMtt = 99999999
+    # for i in range(len(pointsPlotted)):
+    #     if masterParamap[pointsPlotted[i][0], pointsPlotted[i][1]][3] != 0:
+    #         if masterParamap[pointsPlotted[i][0], pointsPlotted[i][1]][0] > maxAuc:
+    #             maxAuc = masterParamap[pointsPlotted[i][0],pointsPlotted[i][1]][0]
+    #         if masterParamap[pointsPlotted[i][0],pointsPlotted[i][1]][0] < minAuc:
+    #             minAuc = masterParamap[pointsPlotted[i][0],pointsPlotted[i][1]][0]
+    #         if masterParamap[pointsPlotted[i][0],pointsPlotted[i][1]][1] > maxPe:
+    #             maxPe = masterParamap[pointsPlotted[i][0],pointsPlotted[i][1]][1]
+    #         if masterParamap[pointsPlotted[i][0],pointsPlotted[i][1]][1] < minPe:
+    #             minPe = masterParamap[pointsPlotted[i][0],pointsPlotted[i][1]][1] 
+    #         if masterParamap[pointsPlotted[i][0],pointsPlotted[i][1]][2] > maxTp:
+    #             maxTp = masterParamap[pointsPlotted[i][0],pointsPlotted[i][1]][2]
+    #         if masterParamap[pointsPlotted[i][0],pointsPlotted[i][1]][2] < minTp:
+    #             minTp = masterParamap[pointsPlotted[i][0],pointsPlotted[i][1]][2]
+    #         if masterParamap[pointsPlotted[i][0],pointsPlotted[i][1]][3] > maxMtt:
+    #             maxMtt = masterParamap[pointsPlotted[i][0],pointsPlotted[i][1]][3]
+    #         if masterParamap[pointsPlotted[i][0],pointsPlotted[i][1]][3] < minMtt:
+    #             minMtt = masterParamap[pointsPlotted[i][0],pointsPlotted[i][1]][3]
+
+
+    affine = np.eye(4)
+    niiarray = nib.Nifti1Image(masterParamap, affine, dtype=np.double)
+    if os.path.exists(destinationPath):
+        os.remove(destinationPath)
+    nib.save(niiarray, destinationPath)
+    print("Total time taken (sec):", time.time() - start)
+    # print([minAuc, maxAuc, minPe, maxPe, minTp, maxTp, minMtt, maxMtt])
+
 def main(imPath, maskPath, windowHeightValue, windowWidthValue, windowDepthValue, destinationPath):
     start = time.time()
     if not os.path.exists(imPath):
@@ -273,4 +587,5 @@ def main(imPath, maskPath, windowHeightValue, windowWidthValue, windowDepthValue
 # Argument 4 --> Y Voxel Dim (default value 4) (mm --> assumes pixdim in NIFTI header uses mm units)
 # Argument 5 --> Z Voxel Dim (default value 4) (mm --> assumes pixdim in NIFTI header uses mm units)
 # Argument 6 --> Absolute path of paramap destination file (in NIFTI format)
-main(sys.argv[1], sys.argv[2], float(sys.argv[3]), float(sys.argv[4]), float(sys.argv[5]), sys.argv[6])
+# main(sys.argv[1], sys.argv[2], float(sys.argv[3]), float(sys.argv[4]), float(sys.argv[5]), sys.argv[6])
+main2d("/Volumes/CREST Data/David_S_Data/Prostate Sample Data/85300606_20230814_093203_0000_CEUS.nii.gz", "/Volumes/CREST Data/David_S_Data/Prostate Sample Data/nifti_segmentation_QUANTUS/const_85300606_20230814_093203_0000_CEUS.nii.gz", 6, 6, "/Volumes/CREST Data/David_S_Data/Prostate Sample Data/nifti_segmentation_QUANTUS/paramap_85300606_20230814_093203_0000_CEUS.nii.gz", 1, 0, 30)
